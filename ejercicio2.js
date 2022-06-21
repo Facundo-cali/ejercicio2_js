f808ac80e870fc15d79af43e95bfd966bba9b257
Facundo-cali <facundocali1234@gmail.com>
2022-06-21 18:53:17 -0300

    ejercicio 2 curso js

---
+++ ejercicio2.js
@@ -0,0 +1,2 @@
+//ejercicio 2 curso js
+const lista =["Facundo", 21, false, new Date(2001, 0, 8), {titulo : "El amor en los tiempos del cólera", autor : "Gabriel García Márquez", fecha: 1985, url: "https://es.wikipedia.org/wiki/El_amor_en_los_tiempos_del_c%C3%B3lera"}];
